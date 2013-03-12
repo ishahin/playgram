4294bb6a30dbc509008e5d2f6a14da2a8e64da53
Massimiliano Mazzarolo <massimiliano.mazzarolo@gmail.com>
2013-03-12 23:53:21 +0100

    Added SecureSocial configuration

--- project/Build.scala
+++ project/Build.scala
@@ -4,18 +4,32 @@ import play.Project._
 
 object ApplicationBuild extends Build {
 
-  val appName         = "playgram"
-  val appVersion      = "1.0-SNAPSHOT"
+  val appName = "playgram"
+  val appVersion = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
     // Add your project dependencies here,
     jdbc,
-    anorm
+    anorm,
+    SecureSocial.dependency
   )
 
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
+
+    resolvers += SecureSocial.repository
     // Add your own project settings here      
   )
 
+}
+
+object SecureSocial extends Build {
+
+  private val version = "master-SNAPSHOT"
+
+  private val target = if (version.endsWith("SNAPSHOT")) "snapshots" else "releases"
+
+  val repository = Resolver.url("Typesafe Community Repository", url("http://repo.scala-sbt.org/scalasbt/sbt-plugin-"+target))(Resolver.ivyStylePatterns)
+
+  val dependency = "securesocial" %% "securesocial" % version
 }
